00d48247782e657532bad690e7ab6297c4bec135
Cameron Clough <cameronjclough@gmail.com>
2022-08-09 17:15:48 +0100

    feat(api): auth for dongle endpoint

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -5,7 +5,7 @@ export type Secret =
   | Buffer
   | { key: string | Buffer; passphrase: string };
 
-export async function verify(token: any, key: Secret) {
+export async function verify(token: string, key: Secret) {
   try {
     const result = verifyToken(token.replace('JWT ', ''), key, { algorithms: ['RS256'], ignoreNotBefore: true });
     return result;
--- src/pages/api/v1.1/devices/:dongleId/index.ts
+++ src/pages/api/v1.1/devices/:dongleId/index.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../../../lib/prisma';
+import { Secret, verify } from '../../../../../lib/jwt';
 
 /**
  * GET /v1.1/devices/:dongleId
@@ -9,13 +10,43 @@ import prisma from '../../../../../lib/prisma';
  * Returns an object representing a comma device.
 */
 export default async (req: NextApiRequest, res: NextApiResponse<Api.Response<Api.Device>>) => {
-  // TODO: user guard and filter by account
-  const device = await prisma.device.findFirst({ where: { dongleId: req.query.dongleId as string } });
+  const { authorization } = req.headers;
+  if (!authorization) {
+    res.status(401).json({
+      code: 401,
+      error: 'Unauthorized',
+      details: 'Missing authorization header',
+    });
+    return;
+  }
+
+  const { dongleId } = req.query;
+  if (!dongleId || typeof dongleId !== 'string') {
+    res.status(400).json({
+      code: 400,
+      error: 'Bad Request',
+      details: 'Missing required parameter: dongleId',
+    });
+    return;
+  }
+
+  const device = await prisma.device.findFirst({ where: { dongleId } });
   if (!device) {
     res.status(404).json({
       code: 404,
       error: 'Not Found',
       details: 'Device not found',
+    });
+    return;
+  }
+
+  // TODO: check user auth with next-auth and check if device owner
+  const decoded = await verify(authorization, device.publicKey as Secret);
+  if (!decoded) {
+    res.status(401).json({
+      code: 401,
+      error: 'Unauthorized',
+      details: 'Invalid authorization token',
     });
     return;
   }
